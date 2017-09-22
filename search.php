89c39eeccb5ef23950e5f53d48b3cc1744a97d95
cyco57 <cyril231275@gmail.com>
2017-09-22 21:38:19 +0200

    ajout d'un commentaire dans le fichier search.php

--- search.php
+++ search.php
@@ -1,4 +1,6 @@
 <?php
+// petit script pour récupérer le contenu du champ de recherche
+// et revenir à la page permettant d'afficher le résultat
 $arg = 'location:../begin.php?choix=recherche&search='.$_POST['search'].'&nav=1';
 
 header($arg);
